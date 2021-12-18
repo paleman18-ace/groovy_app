7c87379233d658ea3f764c8913dca0448f5e8420
yowayowamac <jetbracket2471@gmail.com>
2021-12-18 12:51:06 +0900

    html表示

--- app.groovy
+++ app.groovy
@@ -5,4 +5,13 @@ class App {
     def home(){
         "Hello!!"
     }
+
+    @RequestMapping("/site")
+    def site(){
+      def header = "<html><body>"
+      def footer = "</body></html>"
+      def content = "<h1>youkoso, site!!</h1><p>nakami</p>"
+      
+      header + footer + content
+    }
 }
